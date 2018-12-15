74196afce3713d7f4c04c6c67d03c9705a3340a7
John Peters <japaters@pacbell.net>
2018-12-15 15:20:48 -0800

    Create nes-compiled.f

---
+++ bids/nes-compiled.f
@@ -0,0 +1,98 @@
+( NES-150 )
+: see ( <name> -- )     \ version of 'see' to NOT show file name where defined
+  >in @  see >in ! cr ( .viewinfo 2drop cr ) ;
+
+: HEADLINE ( -- )
+  31 spaces ." Only       Part       Parts      Time       Labor      Sell "
+  cr ;
+
+: RISER-100 ( - )
+  [ electric ] keyboard on kclr
+\ setup  hot-220
+1 [ RISER  ] 100A   cr
+1 [ 90-INC ] 100A   cr
+1 [ LB-INC ] 100A   cr
+headline total ;
+
+: RISER-125 ( - )
+  electric keyboard on kclr
+1 [ RISER  ] 125A    cr
+1 [ 90-INC ] 125A    cr
+1 [ LB-INC ] 125A    cr
+headline total ;
+
+: RISER-150 ( - )
+  electric keyboard on kclr
+1 [ RISER  ] 150A    cr
+1 [ 90-INC ] 	150A   cr
+1 [ LB-INC ] 	150A   cr
+headline total ;
+
+: RISER-200 ( - )
+  electric keyboard on kclr
+1 [ RISER  ] 200A  cr
+1 [ 90-INC ] 200A  cr
+1 [ LB-INC ] 200A  cr
+headline total ;
+
+: NES-150 ( 150A 2 meter New Service )
+( SETUP 	HOT-202     edfined but it is there )
+  electric keyboard on kclr
+1 [ setup   ]  job    cr
+1 [ RISER   ]  	150A  	cr
+1 [ 90-INC  ]  	150A  cr	
+2 [ LB-INC  ]  	150A  cr
+1 [ METER   ]  2-gang cr
+2 [ OS-NIP  ]  	100A  cr
+1 [  PANEL  ]  125A-12-24 cr
+1 [ C-B     ]  	100A  	cr
+1 [ GROUND  ]  	BOND  cr
+1 [ GROUND  ]  	150A  cr
+headline total ;
+
+\ Permit  riser  meter-main or meter-panel  grounding  reconnections
+
+: NES-INC-100A  ( -- )
+  electric keyboard on kclr
+1  [ permit ] permit PANEL-CHANGE CR
+1  [ riser ]  riser  RISER-100      CR
+1  [ METER-RT ]  100A CR
+1  [ C-B ]       100A   CR
+1  [ PANEL ]      150a-16-32 CR
+3  [  RECONNECT ]   1/2          CR
+1  [ RECONNECT ]   6-LOOM-N&T     CR
+1  [ GROUND ]     BOND             CR
+1  [ GROUND ]     ROD                CR
+HEADLINE TOTAL ;
+
+\ Permit  riser  meter-main or meter-panel  grounding  reconnections
+
+\ NES-INC-100A  ( Note the A at the end )
+
+: RISER-100A  108.23 w/e 528 c ;
+
+\s
+
+1 RISER-100A
+1 METER-RT  100A
+1 C-B       100A
+1 PANEL     150a-16-32
+3 RECONNECT 1/2
+1 RECONNECT 6-LOOM-N&T
+1 GROUND    BOND
+1 GROUND    ROD
+1 PERMIT PANEL-CHANGE
+Total  cr cr
+
+cr 1 RISER-100A
+
+
+cr 1 METER-RT  100A
+cr 1 C-B       100A
+cr 1 PANEL     150a-16-32
+cr 3 RECONNECT 1/2
+cr 1 RECONNECT 6-LOOM-N&T
+cr 1 GROUND    BOND
+cr 1 GROUND    ROD
+cr 1 PERMIT PANEL-CHANGE
+cr Total  cr cr
