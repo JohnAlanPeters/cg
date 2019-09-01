99b13eb8efc299893924e4aa5414902cfb57f093
John Alan Peters <japeters@pacbell.net>
2019-09-01 11:25:00 -0700

    webby clean and fix
    output on same line as input in browser;
    headers to browser including content-length.

--- src/vectint.f
+++ src/vectint.f
@@ -1,18 +1,18 @@
 
-create vbuf 200 1024 * allot
-variable vec
+create vbuf 4 1024 * allot
+2variable vec   \ save source to restore after interpret
 
-: pplace ( addr cnt buf -- )  \ add text to word counted buffer
-  dup >r 2 + r@ w@ + swap >r r@ cmove
-  r> r@ w@ + r> w! ;
+: wplace ( addr cnt buf -- )  \ add text to word counted buffer
+  over >r dup>r wcount + swap cmove r> r> swap w+! ;
 
 : vQUERY ( addr cnt -- )
+  (source) 2@ vec 2!
   (SOURCE) 2! >IN OFF 0 TO SOURCE-ID 0 TO SOURCE-POSITION ;
 
 : vtype ( addr cnt -- )
-  vbuf pplace s"  " vbuf pplace ;  \ add text to a counted string
+  vbuf wplace s"  " vbuf wplace ;  \ add text to word counted buffer
 
-: hcr crlf$ count vbuf pplace ;
+: hcr crlf$ count vbuf wplace ;
 
 : ?vcr ( n -- )
   ?dup if 64 mod 0= if hcr then then ;
@@ -20,35 +20,38 @@ variable vec
 : 2crlfs ( addr len -- addr len )
    crlf$ count vbuf place crlf$ count vbuf +place
    vbuf count search -1 =
-   if 4 - swap 4 + swap else 2drop 0  0 then ;
+   if 4 - swap 4 + swap else 2drop 0 0 then ;
 
 \ interpret input from a string; result in a string
 : vectint ( addr cnt -- addr cnt)  \ vectored interpret
   0 vbuf w!
-  ['] vtype is type
-  ['] hcr is cr
-  ['] ?vcr is ?cr
-  vquery
-  ['] interpret
-  catch
-  [ hidden ] ['] c_type is type ['] c_cr is cr
-  ['] c_?cr is ?cr
-   ?dup if ." error " . then
-  \ vbuf >r r@ w@ r> 2 + swap dup 0= if 2drop s" ok" then
-  s"  ok " vbuf pplace vbuf >r r@ w@ r> 2 + swap
-   ;
+   ['] vtype is type
+   ['] hcr is cr
+   ['] ?vcr is ?cr
+   vquery
+   ['] _interpret
+   catch
+    vec 2@ (source) 2!
+    [ hidden ] ['] c_type is type ['] c_cr is cr
+    ['] c_?cr is ?cr
+    0 ?dup if ." error " . then
+    s"  ok " vbuf wplace crlf$ count vbuf wplace vbuf wcount ;
 
 : sendline ( addr cnt -- )
   ssock WriteSocketLine drop ;
 
-: scontentlen ( len -- )
+: sendheaders ( clen -- )        \ headers to browser including content-length
+   s" HTTP/1.1 200 OK" sendline
+   s" Content-type: text/html" sendline
+   s" Server: Forth" sendline
    s" Content-length: " b2sock
    0 (d.) sendline crlf$ count b2sock ;
+   \ 'sendline' sends 1 crlf; 2nd empty line needed before data
 
 : sendfile ( addr cnt -- )
    r/o open-file not
    if >r vbuf 2048 r@ read-file not
-      if  dup scontentlen
+      if  dup sendheaders
           vbuf swap b2sock
       else drop then r> close-file
    then drop ;
@@ -58,16 +61,13 @@ variable vec
    \ first line has GET <path> HTTP
    \ else client wants forth executed
    \ get past headers to data
-   s" HTTP/1.1 200 OK" sendline
-   s" Content-type: text/html" sendline
-   s" Server: Forth" sendline
    over 3 s" GET" compare not
    if 2drop
      s" \cg\src\webinterpret\webinterpret-f.html" sendfile
-   else 2crlfs 2dup type cr \ remove headers
-        vectint dup scontentlen  b2sock
+   else 2crlfs ?dup if 2dup type cr \ remove headers
+        vectint 2dup type dup sendheaders b2sock
+       else drop then
    then ;
-
 
 
 
