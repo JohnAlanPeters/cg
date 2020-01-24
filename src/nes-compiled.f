69534e5ae145f5f84d4c775dd47323f8c3d3ce1c
John Alan Peters <japeters@pacbell.net>
2020-01-23 18:15:40 -0800

    Various price updates and more

--- src/nes-compiled.f
+++ src/nes-compiled.f
@@ -4,56 +4,57 @@
   31 spaces ."            Part       Parts      Time       Labor      Sell "                                
   cr ;
 
-: RISER-100 ( - )
-  [ electric  ] keyboard on kclr   cr 
+: RISER-100A ( - )
+  [ electric  ] keyboard on kclr   cr
 1 [ SETUP     ] ." Setup"  hot-220 cr
 1 [ RISER cr  ] ." Riser"  100A    cr
 1 [ 90-INC cr ] ." 90-inc" 100A    cr
 1 [ LB-INC cr ] ." LB-inc" 100A    cr
 headline total ;
 
-: RISER-125 ( - )
+: RISER-125A ( - )
   electric keyboard on kclr
 1 [ RISER remvoc ] 125A    cr
 1 [ 90-INC remvoc ] 125A    cr
 1 [ LB-INC remvoc ] 125A    cr
 headline total ;
 
-: RISER-150 ( - )
+: RISER-150A ( - )
   electric keyboard on kclr
 1 [ RISER  ] 150A    cr
 1 [ 90-INC ] 	150A   cr
 1 [ LB-INC ] 	150A   cr
 headline total ;
 
-: RISER-200 ( - )
+: RISER-200A ( - )
   electric keyboard on kclr
 1 [ RISER  ] 200A  cr
 1 [ 90-INC ] 200A  cr
 1 [ LB-INC ] 200A  cr
-headline total ;
+         total ;
 
-: NES-150 ( 150A 2 meter New Service )
+: NES-150A ( 150A 2 meter New Service )
 ( SETUP 	HOT-202     edfined but it is there )
-  electric keyboard on kclr
-1 [ setup   ]  job        cr
-1 [ RISER   ]  	150A      	cr
-1 [ 90-INC  ]  	150A      cr	
-2 [ LB-INC  ]  	150A      cr
-1 [ METER   ]  2-gang     cr
-2 [ OS-NIP  ]  	100A      cr
-1 [  PANEL  ]  125A-12-24 cr
-1 [ C-B     ]  	100A      	cr
-1 [ GROUND  ]  	BOND      cr
-1 [ GROUND  ]  	150A      cr
-headline total ;
+  electric keyboard on kclr         cr
+1 [ setup   ] ." Setup"  job        cr
+1 [ RISER   ] ." Riser"  	150A      	cr
+1 [ 90-INC  ] ." 90-inc" 	150A      cr	
+2 [ LB-INC  ] ." LF-inc" 	150A      cr
+1 [ METER   ] ." Meter"  2-gang     cr
+2 [ OS-NIP  ] ." OS-Nip" 	100A      cr
+1 [ PANEL   ] ." Panel"  125A-12-24 cr
+1 [ C-B     ] ." CB"     	100A      	cr
+1 [ GROUND  ] ." Ground Bond" 	BOND      cr
+1 [ GROUND  ] ." Ground"  	150A      cr
+         total ;
 
 \ permit  riser  meter-main or meter-panel  grounding  reconnections
 
-: NES-100-INC  ( -- )
+: NES-100A  ( -- )
   electric keyboard on kclr            CR
 1  [ permit ] ." permit" PANEL-CHANGE   
-1  [ riser ] ( not needed ) RISER-100  CR \ the part name comes from prior
+1  [ riser ] ( not needed )
+                   RISER-100A  CR \ the part name comes from prior
 1  [ METER-RT ]    100A                CR
 1  [ C-B ]         100A                CR
 1  [ PANEL ]       150a-16-32          CR
@@ -65,7 +66,7 @@ HEADLINE TOTAL ;
 
 \ Permit  riser  meter-main or meter-panel  grounding  reconnections
 
-\ NES-INC-100A  ( Note the A at the end )
+
 
 : RISER-100A  108.23 w/e 528 c ;
 
