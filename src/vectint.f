62a8946b9f14ad7d5a1f5b4e12a029500968ee16
John Alan Peters <japeters@pacbell.net>
2020-06-10 15:48:37 -0700

    web i/o to file
    file: src\webinterpret\users

--- src/vectint.f
+++ src/vectint.f
@@ -38,6 +38,7 @@ defer to-web
   200 ms
   crlf$ 2 + 1 vbuf wplace     \ add newline=linefeed=decimal-10=hex-0a
   vbuf wcount dup 2 sendheaders
+  2dup data>fuser
   b2sock                      \ send the response to the socket
   conscol off 0 vbuf w!       \ ready for next line
   sockread chkcntnu ;         \ get continue request from webpage
@@ -116,10 +117,10 @@ defer to-web
      s" \cg\src\webinterpret\webinterpret-f.html" sendfile
    else
      2crlfs              \ chop off headers up to 2 CRLFs to get to data
-     \ 2dup type         \ type the forth command to the surface console
+     2dup data>fuser ( type )        \ type the forth command to the surface console
      vectint             \ get output of request into buffer
 
-     \ cr 2dup type      \ display response in console
+     2dup data>fuser  \ cr 2dup type      \ display response in console
      dup sentcr if 5 else 0 then sendheaders   \ send the HTML headers
      b2sock             
  \ send the response to the socket
