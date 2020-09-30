9a82c3835b994453f7bb88934ea9913ab8d4a47e
John Alan Peters <japeters@pacbell.net>
2020-09-30 10:07:40 -0700

    restart on the hour

--- timeweb.bat
+++ timeweb.bat
@@ -1,6 +1,10 @@
-rem restart webby every so often
+rem restart webby every hour
+taskkill /f /im cg.exe
+start /b "" \cg\cg.exe 0 do-server
 :loop
-   taskkill /f /im cg.exe
-   start /b "" \cg\cg.exe 0 do-server
-   timeout 3600
+   if "%time:~3,2%" == "00" (
+     taskkill /f /im cg.exe
+     start /b "" \cg\cg.exe 0 do-server
+     timeout 3540
+   ) else ( timeout 30 )
 goto loop
