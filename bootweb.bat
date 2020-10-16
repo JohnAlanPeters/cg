043927ec3dae7c18dcf3dabf8112455848208872
John Alan Peters <japeters@pacbell.net>
2020-10-16 11:38:06 -0700

    restart on the hour

--- bootweb.bat
+++ bootweb.bat
@@ -1,8 +1,11 @@
 rem restart webby every so often
 rem delay on boot
 timeout 30
+start /b "" \cg\cg.exe 0 do-server
 :loop
-   taskkill /f /im cg.exe
-   start /b "" \cg\cg.exe 0 do-server
-   timeout 3600
+   if "%time:~3,2%" == "00" (
+     taskkill /f /im cg.exe
+     start /b "" \cg\cg.exe 0 do-server
+     timeout 3540
+   ) else ( timeout 30 )
 goto loop
